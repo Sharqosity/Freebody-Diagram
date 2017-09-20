96b210063d44f711f6f5a0ff7def7f493854cc65
Sharqosity <typingpalguy@gmail.com>
2017-09-20 09:20:50 -0400

    add undo/clear buttons, commented panel class

--- src/Panel.java
+++ src/Panel.java
@@ -3,6 +3,8 @@ import javafx.scene.layout.Pane;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -13,35 +15,70 @@ import java.util.ArrayList;
  */
 public class Panel extends JPanel {
 
+    private JButton clearButton = new JButton("Clear");
+    private JButton undoButton = new JButton("Undo");
+    private Image img;
+    private ArrayList<Vector> vectors = new ArrayList<>();
+
     public Panel(){
+        //adds mouse listener, find the methods at the bottom
         this.addMouseListener(new CustomMouseListener());
 
+        //button that clears vectors in array list
+        clearButton.addActionListener(e -> {
+            vectors.clear();
+            repaint();
+        });                  //x, y, width, height
+        clearButton.setBounds(25, 725, 75, 30);
+        this.add(clearButton);
+
+        //button that removes last vector in array
+        undoButton.addActionListener(e -> {
+            if (!vectors.isEmpty()) {
+                vectors.remove(vectors.size()-1);
+                repaint();
+            }
+        });
+        undoButton.setBounds(125, 725, 75, 30);
+        this.add(undoButton);
+
+        //background
         try{
             img = ImageIO.read(new File("Res/Vector Meme.jpg"));
         }catch(Exception e){
             e.printStackTrace();
         }
 
+        //rad/deg combobox
         String[] dd = {"Radians", "Degrees"};
         JComboBox<String> selector = new JComboBox<String>(dd);
         selector.setBounds(20, 20, 101, 20);
         add(selector);
     }
-    Image img;
-    ArrayList<Vector> vectors = new ArrayList<>();
-
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
+
+        //background
         g2.drawImage(img, 0, 0, getWidth(), getHeight(), null);
+
+        //middle dot
         g.fillOval(getWidth()/2-2, getHeight()/2-2, 5, 5);
+
+        //anti-aliasing
         RenderingHints rh = new RenderingHints(
                 RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON
         );
         g2.setRenderingHints(rh);
-        System.out.println(vectors.size());
+
+        //resultant (draw first so it appears under)
+        g2.setStroke(new BasicStroke(1));
+        g2.setColor(Color.RED);
+        Resultant().draw(g2);
+
+        //vectors
         for (Vector v:vectors) {
             g2.setColor(Color.black);
             g2.setStroke(new BasicStroke(2));
@@ -49,20 +86,17 @@ public class Panel extends JPanel {
             v.draw(g2);
         }
 
-        g2.setColor(Color.RED);
-        Resultant().draw(g2);
     }
-    public Vector Resultant(){
+
+    private Vector Resultant(){
         double sumxcomp = 0;
         double sumycomp = 0;
         for (int i = 0; i < vectors.size(); i++) {
             sumxcomp += vectors.get(i).xComp;
             sumycomp += vectors.get(i).yComp;
-
         }
         Vector res = new Vector(getWidth()/2 + sumxcomp, getHeight()/2 + sumycomp, getWidth()/2, getHeight()/2);
         return res;
-
 
     }
 
