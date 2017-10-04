c848015387b63f0ebedc7f87873c49287623e14e
Andrew Ward <andrew_ward@student.wayland.k12.ma.us>
2017-10-04 10:07:22 -0400

    more useful window size

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * Created by student on 9/15/17.
@@ -11,11 +10,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int Width = 500;
-        int Height = 800;
+        int width = 1000;
+        int height = 800;
         JFrame frame = new JFrame("Freebody Diagram Simulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(0,0,Width,Height);
+        frame.setBounds(0,0,width,height);
 
         Panel p = new Panel();
         frame.add(p);
